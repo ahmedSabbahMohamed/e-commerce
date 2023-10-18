6764a65f85f22a850b70e33d6d5f13c918f20f71
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2023-10-18 20:36:51 +0300

    setting up the Categories component

---
+++ src/components/Categories.jsx
@@ -0,0 +1,17 @@
+import SectionHeader from "./SectionHeader"
+
+function Categories() {
+  return (
+    <>
+    <div className="">
+
+            <div className="container">
+
+                <SectionHeader title={"Categories"} header={"Browse By Category"} />
+            </div>
+    </div>
+    </>
+  )
+}
+
+export default Categories
