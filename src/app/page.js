91b2d86e59ac9ed8697cfe751fc34b5c558b6611
ahmedSabbahMohamed <ahmedsabbah880@gmail.com>
2023-10-18 20:36:08 +0300

    added Today and Categories components to page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
+import Categories from "@/components/Categories";
 import Hero from "@/components/Hero";
+import Today from "@/components/Today";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Today />
+      <Categories />
     </>
   )
 }
